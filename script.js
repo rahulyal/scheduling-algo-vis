7310594eaf4254666272fdcf9eb6da16d710ec74
ikumar21 <ikumar@scu.edu>
2023-03-12 14:40:58 -0700

    Gantt Chart

--- script.js
+++ script.js
@@ -41,6 +41,8 @@ function calculateAverage(){
     console.log(arrivalTimes, cpu, io);
     var [responseTimes,turnaroundTimes,completionTimes,waitingTimes,cpuStartTimes,cpuEndTimes,cpuSums]=sjfScheduling(arrivalTimes, cpu, io)
     outputTableData(responseTimes,turnaroundTimes,completionTimes,waitingTimes,cpuStartTimes,cpuEndTimes,cpuSums);
+    var [ioStartTimes,ioEndTimes]=generateIO(cpuStartTimes,cpuEndTimes)
+    drawGanttChart(cpuStartTimes,cpuEndTimes,ioStartTimes,ioEndTimes)
 }
 
 function getDataArray(){//Stores data into arrays
@@ -184,3 +186,148 @@ function outputTableData(responseTimes,turnaroundTimes,completionTimes,waitingTi
     processOutputTable.innerHTML+=rowContent
   }
 
+function drawGanttChart(cpuStart,cpuStop,ioStart,ioStop){
+  var lowestTime = findLowest(cpuStart)
+  var highestTime = findHighest(ioStop, cpuStop)
+  console.log(lowestTime,highestTime,"TIMES")
+  deltaTime = highestTime-lowestTime;
+  pixelPerSecond = Math.floor(1260.00/deltaTime)
+  var maxWidthGantt=0
+  var startYPointGantt = 5;
+  var scrollTable = (pixelPerSecond<1)
+  timeProp="height=\"30\""
+  if(scrollTable){//Add hor. scroll:More space needed for Gantt
+    maxWidthGantt = deltaTime+140
+    document.getElementById("ganttChartInsert").style.overflowx = "scroll";
+    pixelPerSecond=1
+  }else{
+    maxWidthGantt=1400
+    document.getElementById("ganttChartInsert").style.overflowx = "hidden";
+  }
+  var maxHeightGantt = 40*numberProcesses+30
+  var ganttChartTable = document.getElementById("ganttChartInsert");
+  ganttChartTable.innerHTML=""
+  var ganttContent = "<svg class=\"ganntCPU\" width=\""+String(maxWidthGantt+String(highestTime).length*13-25)+ "\"height=\""+String(maxHeightGantt)+"\">"
+  for(i=0;i<numberProcesses;i++){//Add Gantt Rows
+    ganttContent+="<rect class =\"gannt"+String(i%2==0)+"Row\" width=\""+String(maxWidthGantt-25)+"\" height=\"40\" x = \"5\" y = \"" +String(startYPointGantt+i*40)+"\"/>"
+    ganttContent+="<text x=\"15\" y=\""+String(25+startYPointGantt+i*40)+ " font-size=\"20px\" fill=\"Black\">Process "+String(i+1)+"</text>"
+    for(j = 0;j<cpuStart[i].length;j++){//Create CPU Blocks
+      var wiCPU = String((cpuStop[i][j]-cpuStart[i][j])*pixelPerSecond)
+      var xCPU = (cpuStart[i][j]-lowestTime)*pixelPerSecond+115
+      console.log(wiCPU,i,pixelPerSecond)
+      ganttContent+="<rect class=\"ganntCPU\" width=\""+wiCPU+"\" height=\"30\" x = \""+xCPU+"\" y = \""+String(5+startYPointGantt+40*i)+"\" />"
+    } 
+    for(var j =0;j<ioStop[i].length;j++){//Create IO Blocks
+      var wiIO = String((ioStop[i][j]-ioStart[i][j])*pixelPerSecond)
+      var xIO = (ioStart[i][j]-lowestTime)*pixelPerSecond+115
+      ganttContent+="<rect class=\"ganntIO\" width=\""+wiIO+"\" height=\"30\" x = \""+xIO+"\" y = \""+String(5+startYPointGantt+40*i)+"\" />"
+    }
+  }
+  //Create Lines:
+  yStartingLine = "y1=\""+String(numberProcesses*40+startYPointGantt+1)+"\""
+  yEndingLine = " y2=\""+String(numberProcesses*40+startYPointGantt+25)+"\""
+  lineStringData =  "stroke-dasharray=\"5,5\" d=\"M5 40 l215 0\"/>"
+  //First Line with Starting Time
+  ganttContent+="<line class = \"lineGantt\""+"x1=\"113\" x2=\"113\" y1=\""+String(startYPointGantt)+"\""+yEndingLine+lineStringData
+  ganttContent+="<text x=\""+String(118)+"\" y=\""+String(20+startYPointGantt+numberProcesses*40)+ " font-size=\"15px\" fill=\"Gray\">"+String(lowestTime)+"</text>"
+  if(scrollTable){//Create evenly spaced Time Lines
+    var pixelLocation = 150
+    while(pixelLocation<=deltaTime-100){
+      var x = 115+pixelLocation
+      xPos = "x1=\""+String(x)+"\" x2=\""+String(x)+"\""
+      ganttContent+="<line class = \"lineGantt\""+xPos +yStartingLine+yEndingLine+ lineStringData
+      ganttContent+="<text x=\""+String(x+5)+"\" y=\""+String(20+startYPointGantt+numberProcesses*40)+ " font-size=\"15px\" fill=\"Gray\">"+String(pixelLocation)+"</text>"
+      pixelLocation+=150
+    }
+  }else{
+    interval = findInterval(deltaTime)
+    timeSpace = Math.floor(deltaTime/interval)
+    for(var i=1;i<interval;i++){//Create Evenly spaced Time Lines
+      var x = 115+i*timeSpace*pixelPerSecond
+      xPos = "x1=\""+String(x)+"\" x2=\""+String(x)+"\""
+      ganttContent+="<line class = \"lineGantt\""+xPos +yStartingLine+yEndingLine+ lineStringData
+      ganttContent+="<text x=\""+String(x+5)+"\" y=\""+String(20+startYPointGantt+numberProcesses*40)+ " font-size=\"15px\" fill=\"Gray\">"+String(lowestTime+i*timeSpace)+"</text>"
+    }
+  }
+  //Last Line with Ending Time
+  x = 115+deltaTime*pixelPerSecond
+  xPos = "x1=\""+String(x)+"\" x2=\""+String(x)+"\""
+  ganttContent+="<line class = \"lineGantt\""+xPos +yStartingLine+yEndingLine+ lineStringData
+  ganttContent+="<text x=\""+String(x+5)+"\" y=\""+String(20+startYPointGantt+numberProcesses*40)+ " font-size=\"15px\" fill=\"Gray\">"+String(highestTime)+"</text>"
+  ganttContent+="</svg>";
+  ganttChartTable.innerHTML+=ganttContent
+  console.log("HI")
+
+
+
+}
+
+function generateIO(cpuStart,cpuStop,){
+    var ioTimesCopy=[]
+    for(var i =0;i<numberProcesses;i++){
+      ioTimesCopy.push(ioTimes[i].filter(function(item){return !isNaN(item)}))
+    }
+    ioStart=[]
+    ioStop=[]
+    for(var i =0;i<numberProcesses;i++){//For each process
+      ioStart.push([])
+      ioStop.push([])
+      for(j = 0;j<ioTimesCopy[i].length;j++){//For each IO Time, add start and stop
+        ioStart[i].push(cpuStop[i][j])
+        ioStop[i].push(cpuStop[i][j]+ioTimesCopy[i][j])
+      }
+    }
+    console.log(ioStart)
+    console.log(ioStop)
+    return [ioStart,ioStop]
+  }
+
+function findLowest(arr){
+  lowestValue = arr[0][0];
+  for(var i = 0;i<arr.length;i++){
+    for(var j=0;j<arr[i].length;j++){
+      if(lowestValue>arr[i][j]){
+        lowestValue = arr[i][j]
+      }
+    }
+  }
+  return lowestValue
+}
+
+function findHighest(arr1,arr2){
+    highestValue=0  
+    for(var i = 0;i<arr1.length;i++){
+        for(var j=0;j<arr1[i].length;j++){
+            if(highestValue<arr1[i][j]){
+                highestValue = arr1[i][j]
+            }
+        }
+    }
+    for(var i = 0;i<arr2.length;i++){
+        for(var j=0;j<arr2[i].length;j++){
+            if(highestValue<arr2[i][j]){
+                highestValue = arr2[i][j]
+            }
+        }
+    }
+    return highestValue
+}
+
+function findInterval(deltaTime){//Find nice intervals that give less space
+  if(deltaTime<=5){
+    return deltaTime
+  }
+  var i =5.0
+  var interval = 5.0
+  var low = deltaTime/i-Math.floor(deltaTime/i)
+  while(i<=10.0){
+    var lowI = deltaTime/i-Math.floor(deltaTime/i)
+    console.log(lowI,i)
+    if(lowI<=low){
+      low=lowI
+      interval=i
+    }
+    i+=1.000;
+  }
+  return Math.floor(interval);
+}
